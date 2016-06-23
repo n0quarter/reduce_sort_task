fae3746fff8ab4fde29a81de75491bd4bf59f89e
Viktor Shcherban <viktor.shcherban@kaufda.de>
2016-06-23 09:17:06 +0200

    changes

--- sort_task.playground/Contents.swift
+++ sort_task.playground/Contents.swift
@@ -7,23 +7,6 @@ let champs : [Int: String] = [
   1983 : "Ukraine",
 ]
 
-// v2, reduce.
-//let winnersDict = champs.reduce([String: Int]()) { (var winners, pair) in
-//  winners[pair.1] = (winners[pair.1] ?? 0) + 1
-//  return winners
-//}
-
-// just for fun: oneliner
-let winnersDict = champs.reduce([String: Int]()) {
-  var winners = $0
-  winners[$1.1] = (winners[$1.1] ?? 0) + 1
-  return winners
-}
-
-let winnersArray = winnersDict.map { ($0, $1) }.sort{$1.1 < $0.1}
-print(winnersArray)
-
-
 // v1. trivial
 var winners2 = [String:Int]()
 for (year, country) in champs {
@@ -35,34 +18,25 @@ let winnersArray2 = winners2.map { ($0, $1) }.sort{$1.1 < $0.1}
 print(winnersArray2)
 
 
+// v2, reduce with named parameters
+let winnersDict1 = champs.reduce([String: Int]()) { (var winners, pair) in
+  winners[pair.1] = (winners[pair.1] ?? 0) + 1
+  return winners
+}
 
+// just for fun: reduce without named parameters
+let winnersDict = champs.reduce([String: Int]()) {
+  var winners = $0
+  winners[$1.1] = (winners[$1.1] ?? 0) + 1
+  return winners
+}
 
-//var d = ["foo" : "1", "bar" : "2"]
-//let result1 = d.map { (key, value) in (key, value.uppercaseString) }
-//result1
-
-
-
-// v3 !
-//let winnersDict = champs.reduce([String: Int]()) { (var winners, pair) in
-//  if let previousVal = winners[pair.1] {
-//    winners[pair.1] = previousVal+1
-//  }
-//  else {
-//    winners[pair.1] = 1
-//  }
-//  return winners
-//}
-//winnersDict
+let winnersArray = winnersDict.map { ($0, $1) }.sort{$1.1 < $0.1}
+print(winnersArray)
 
 
 
-
-
-
-
-
-// v1, trivial
+// super trivial version
 //var winners : [String:Int] = [String:Int]()
 //for (year, country) in champs {
 //  if let previousVal = winners[country] {
